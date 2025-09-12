f8d55dbe08dc248603ffa9b5d613e2c3cdc35257
AkiKurvinen <aki.kurvinen@hotmail.fi>
2025-09-12 11:05:59 +0300

    adjust MUI theme colors

---
+++ stories/ColorGallery.stories.jsx
@@ -0,0 +1,11 @@
+import { ColorGallery } from './ColorGallery';
+
+const meta = {
+  title: 'Design Tokens/Colors',
+  component: ColorGallery,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Colors = {};
