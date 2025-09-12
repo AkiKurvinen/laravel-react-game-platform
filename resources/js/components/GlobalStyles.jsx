44a0ba137c3c63254fc95f3765a2927aab82741d
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-09-12 16:48:26 +0300

    set theme colors

--- resources/js/components/GlobalStyles.jsx
+++ resources/js/components/GlobalStyles.jsx
@@ -4,20 +4,21 @@ export const GlobalStyles = createGlobalStyle`
 
 body {
   scrollbar-width: thin;
-  scrollbar-color: var(--primary) var(--paper);
+  scrollbar-color: var(--mui-palette-primary-main) var(--mui-palette-background-paper);
   margin: 0;
   padding: 0;
   min-height: 100vh;
+  background-color: var(--mui-palette-background-paper);
 }
 body::-webkit-scrollbar {
   width: 15px;
 }
 body::-webkit-scrollbar-track {
-  background: var(--paper);
+  background: var(--mui-palette-background-paper);
 }
 body::-webkit-scrollbar-thumb {
-  background-color: var(--primary);
+  background-color: var(--mui-palette-primary-main);
   border-radius: 6px;
-  border: 3px solid var(--paper);
+  border: 3px solid var(--mui-palette-background-paper);
 }
 `;
--- resources/js/components/theme.jsx
+++ resources/js/components/theme.jsx
@@ -1,7 +1,14 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-
+    cssVariables: true,
+    palette: {
+        primary: {
+            main: '#a80600',
+            light: '#ff4545',
+            dark: '#530006'
+        }
+    }
 });
 
 export default theme;
